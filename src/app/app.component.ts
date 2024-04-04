abf6fa9fe5aa3c97855e7cda5307f5fcf99b84e6
João Vitor Gonçalves da Costa <goncalvesdacosta05@gmail.com>
2024-04-04 17:27:31 -0300

    updating my first files/folders

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,LoginFormComponent,SignUpFormComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'to_do_list';
+}
